cfc13b042cd455ec2b4ad1f11695be2899bdd686
shravankumar637 <shravankumar637@gmail.com>
2019-09-04 15:51:21 +0530

    initial commit: Implemented sample socket serve and client in nodejs/angularjs usingsocket.io

---
+++ socket-app-client/src/app/document.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Document } from 'src/app/document';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+  currentDocument = this.socket.fromEvent<Document>('document');
+  documents = this.socket.fromEvent<string[]>('documents' );
+
+   t=this.socket.on("documents" , (data) => {
+    console.log("test data:",data);
+    
+  })
+
+  constructor(private socket: Socket) { }
+
+  getDocument(id: string) {
+    this.socket.emit('getDoc', id);
+  }
+
+  newDocument() {
+    this.socket.emit('addDoc', { id: this.docId(), doc: '' });
+  }
+
+  // editDocument(document: Document) {
+  //   this.socket.emit('editDoc', document);
+  // }
+
+  private docId() {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < 5; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  }
+}
+
